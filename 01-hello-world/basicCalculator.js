c559c6e269b88cec1cb9da11ec6e338c976fc44a
Carlos Sanchez Gonzalez <hypnoticdata777@gmail.com>
2025-11-01 20:19:17 -0600

    Commit 2 - Added basic operation logic (switch statement)

--- 01-hello-world/basicCalculator.js
+++ 01-hello-world/basicCalculator.js
@@ -5,3 +5,28 @@ let num2 = 4;
 let operator = "+";
 
 console.log('Calculating: ${num1} ${operator} ${num2}');
+
+let result;
+
+switch (operator) {
+    case "+":
+        result = num1 + num2;
+        break;
+        case "-":
+            result =num1 - num2;
+            break;
+            case"*":
+            result = num1 * num2;
+            break;
+            case "/":
+                if (num2 !==0) {
+                    result = num1 / num2;
+                } else {
+                    result = "Cannot divide by zero!";
+                }
+                break;
+                default:
+                    result = "Invalid operator!";
+                }
+console.log ('Result: ${result}');
+
